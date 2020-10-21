ad00eb2baf853597903726615e9a2197e8c4d0a7
Suku Moorthy <Suku.Moorthy@phoenix.edu>
2020-10-20 23:08:55 -0700

    updated s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/SMOORTHY-training/s3-bucket/aws"
   version = "1.15.0"
   # insert required variables here
-  bucket-prefix="suku-moorthy"
+  bucket_prefix = "suku-moorthy"
 }
