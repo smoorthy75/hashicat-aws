c191ff9eba38aad6b9c603bc79e7420543ae9be2
Suku Moorthy <Suku.Moorthy@phoenix.edu>
2020-10-21 02:44:07 -0700

    updated tags

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "hashicat" {
   enable_dns_hostnames = true
 
   tags = {
-    name = "${var.prefix}-vpc-${var.region}"
+    name        = "${var.prefix}-vpc-${var.region}"
     environment = "Production"
   }
 }
@@ -124,7 +124,9 @@ resource "aws_instance" "hashicat" {
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
 
   tags = {
-    Name = "${var.prefix}-hashicat-instance"
+    Name       = "${var.prefix}-hashicat-instance"
+    Department = "IT"
+    Billable   = "true"
   }
 }
 
